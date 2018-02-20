f608a530a916a4096282d421c45fc7efb5bfafbe
AundreE <32345683+AundreE@users.noreply.github.com>
2018-02-19 20:17:06 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'removed'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
-app.config['MAIL_PORT'] = '465'
+app.config['MAIL_PORT'] = '2525'
 app.config['MAIL_USERNAME'] = 'removed'
 app.config['MAIL_PASSWORD'] = 'removed'
 mail = Mail(app)
